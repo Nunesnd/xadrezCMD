fe2f41cac9a994f0d5389ee1552a6dfd53011acb
Diego Nunes <nunesdiegond@gmail.com>
2022-03-20 15:26:53 -0300

    imprimimos orientacao no tabuleiro, e colorimos as pecas

--- Program.cs
+++ Program.cs
@@ -9,13 +9,16 @@ namespace xadrezConsole
         static void Main(string[] args)
         {
 
-            /*
             try { 
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 4));
                 tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 3));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(0, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(5, 2));
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(2, 1));
 
                 Tela.imprimirTabuleiro(tab);
 
@@ -24,12 +27,13 @@ namespace xadrezConsole
             {
                 Console.WriteLine(e.Message);
             }
-            */
+            /*
 
             PosicaoXadrez pos = new PosicaoXadrez('c', 4);
 
             Console.WriteLine(pos);
             Console.WriteLine(pos.toPosicao());
+            */
 
             Console.WriteLine("Presione enter para fechar..........");
             Console.ReadLine();
