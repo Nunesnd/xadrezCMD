3333f946254d5e279da461b6134333a544abc042
Diego Nunes <nunesdiegond@gmail.com>
2022-03-20 14:50:12 -0300

    inicio dos tratamentos de erros

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -18,10 +18,43 @@
             return pecas[linha, coluna];
         }
 
+        public bool existePeca(Posicao pos)
+        {
+            valiadarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça mna posição desejada.");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha<0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void valiadarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }  
     }
 }
